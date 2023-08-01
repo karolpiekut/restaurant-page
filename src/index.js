ae32c8e4f1061ed6ca8843380430f516d17391cf
kpiekut <karol.piekut@pm.me>
2023-08-01 14:09:56 +0100

    add css file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import _ from 'lodash';
-
+import './style.css';
 function component() {
     const element = document.createElement('div');
-
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
